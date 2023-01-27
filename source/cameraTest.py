6ebdce64d2ec1689062f38906ef8f7f5d9a1492f
apeled <amitp.9787@gmail.com>
2023-01-27 10:27:52 -0800

    Playing with Kivy

--- source/cameraTest.py
+++ source/cameraTest.py
@@ -1,21 +1,21 @@
-import cv2
+import cv2 as cv
 
 # Open the camera
-camera = cv2.VideoCapture(0)
+camera = cv.VideoCapture(0)
 
 while True:
     # Capture frame-by-frame
     ret, frame = camera.read()
 
     # Display the resulting frame
-    cv2.imshow('Camera', frame)
-
+    cv.imshow('Camera', frame)
+    
     # Press 'q' to exit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Release the camera
 camera.release()
 
 # Close all windows
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
